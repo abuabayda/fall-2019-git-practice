fba05d7cc96322da89ec741e4ce28c2130cb0eb5
abuabayda <58151959+abuabayda@users.noreply.github.com>
2020-03-18 20:16:10 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,7 +2,8 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("brave new world");
         System.out.println("abu");
-        System.out.println("change One")
+        System.out.println("change One");
+        System.out.println("Second Change");
 
 
 
